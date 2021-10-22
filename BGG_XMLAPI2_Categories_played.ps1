95e38ad886c83a6ee1bc0f4f4f6394adc3c080ef
Andreas Pettersson <psson@users.noreply.github.com>
2021-10-22 21:34:36 +0200

    first commit

---
+++ BGG_XMLAPI2_Categories_played.ps1
@@ -0,0 +1,24 @@
+﻿$bgguser = 'psson73'
+$year = '2021'
+$bgguserPlaysUri = "https://boardgamegeek.com/xmlapi2/plays?username=$bgguser&mindate=$year-01-01&maxdate=$year-12-31"
+[xml]$xmlBGGUserPlays = Invoke-WebRequest -Uri $bgguserPlaysUri
+
+$uniqueLocations = $xmlBGGUserPlays.plays.play.location | Sort-Object | Get-Unique
+#$uniqueLocations | measure
+
+#$xmlBGGUserPlays.plays.play.item(0).item.name
+
+
+$uniqueGamesPlayed = $xmlBGGUserPlays.plays.play | Select-Object -ExpandProperty item | Select-Object -ExpandProperty objectid | Sort-Object | Get-Unique
+$uniqueGamesPlayed | measure
+
+$gameIDs = ''
+foreach ( $gamePlayed in $uniqueGamesPlayed ) {
+    $gameIDs = $gameIDs + $gamePlayed + ","
+}
+
+#write-host $gameIDs
+$bggGameDataUri = "https://boardgamegeek.com/xmlapi/boardgame/$gameIDs"
+[xml]$gameData = Invoke-WebRequest -Uri $bggGameDataUri
+
+$gameData.boardgames.boardgame | Select-Object -ExpandProperty name | Where-Object { $_.primary -eq 'true' } | Select-Object -ExpandProperty InnerXML | Sort-Object
---
+++ BGG_XMLAPI2_GetBGG10x10Entry.ps1
@@ -0,0 +1,46 @@
+﻿function Get-BGGChallengePlaysForEntry {
+    param (
+        [string]$bggUser,
+        [string[]]$gameIDs,
+        [string]$year
+    )
+
+    $entry = ""
+    $curGameNumber = 1
+
+    foreach ( $gameID in $gameIDs ) {
+           $newRow = Get-BGGChallengePlaysForGame -bggUser $bggUser -gameID $gameID -year $year -gameNumber $curGameNumber
+           $entry = $entry + $newRow
+           $curGameNumber = $curGameNumber + 1
+    }
+
+    return $entry
+}
+
+function Get-BGGChallengePlaysForGame {
+    param ( [string]$bggUser,
+            [string]$gameID,
+            [string]$year,
+            [int]$gameNumber
+    )
+
+    $playsUri = "https://boardgamegeek.com/xmlapi2/plays?username=$bggUser&id=$gameID&mindate=$year-01-01&maxdate=$year-12-31"
+    [xml]$xmlPlays = Invoke-WebRequest -Uri $playsUri
+    $numPlays = 0
+    $paddedGameNumber = $([string]$gameNumber).PadLeft(2,'0')
+    $row = "$paddedGameNumber. "
+    $xmlPlays.plays.play | Sort-Object -Property date,id | Select-Object -First 10 | ForEach-Object { $playStar = "[geekurl=/play/details/$($_.id)]:star:[/geekurl]" ; $row = $row + $playStar ; $numPlays = $numPlays + 1 }
+    $fillerStars = for( $i = $numPlays+1 ; $i -le 10; $i = $i + 1 ) { $row = $row + ':nostar:' }
+    $gameLink = "[thing=$gameID]" + $xmlPlays.plays.play.Item(0).item.name + '[/thing]'
+    $row = $row + $fillerStars + " $gameLink`n"
+    
+    return $row
+
+}
+
+# IDs för spelen som ska hämtas
+$challengeGameIDs = @('146021','167791','118048','822','28143','154203','312484','244522','36218','224037')
+
+# Hämta rad-datat till clipboard
+Get-BGGChallengePlaysForEntry -bggUser 'psson73' -gameIDs $challengeGameIDs -year '2021' | clip
+
