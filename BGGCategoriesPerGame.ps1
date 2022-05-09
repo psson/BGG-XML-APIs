fe4e0e1a352509584232540399894e787c36881a
Andreas Pettersson <psson@users.noreply.github.com>
2022-05-09 20:04:24 +0200

    Major change

---
+++ BGGCategoriesPerGame.ps1
@@ -0,0 +1,59 @@
+﻿function Get-BGGCategoriesForGames {
+    [cmdletbinding()]
+    param (
+        [parameter(ValueFromPipeline)]$gameIDs
+    )
+
+    begin {
+        $categories = @{}
+    }
+
+    process {
+        $thingUri = "https://boardgamegeek.com/xmlapi2/thing?id=$gameID"
+        [xml]$xmlThing = Invoke-WebRequest -Uri $thingUri
+
+        $categoryLinks = $xmlThing | Select-Xml -XPath "//*[@type='boardgamecategory']"
+
+        
+
+        foreach ( $category in $categoryLinks.Node.value ) {
+            try {
+                #Write-host $category
+                $categories.Add($category,'category')
+            } catch [ArgumentException] {
+                # Dublett
+            } catch {
+                Write-Error "Unexpected Error"
+            }
+        }
+    }
+
+    end {
+        return $categories
+    }
+}
+
+
+
+
+
+#$challengeGameIDs = @('36218','164812','244522','2448','161417','50','113294','1927','2243','13223')
+$challengeGameIDs = @('36218','164812','244522','2448','161417','50','113294','1927','2243','13223')
+
+
+$myCats = $challengeGameIDs | Get-BGGCategoriesForGame
+$myCats.Keys | Measure-Object | Select-Object -ExpandProperty count
+
+146021 | Get-BGGCategoriesForGame
+
+$gameID = '146021'
+$thingUri = "https://boardgamegeek.com/xmlapi2/thing?id=$gameID"
+[xml]$xmlThing = Invoke-WebRequest -Uri $thingUri
+
+
+$categoryLinks = $xmlThing | Select-Xml -XPath "//*[@type='boardgamecategory']"
+#$categoryLinks.Node
+
+foreach ( $category in $categoryLinks.Node.value ) {
+    $category
+}
---
+++ Get-GCC10x10.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module psson-BGGAPI -Force
+
+#Användare
+$bggUser='psson73'
+
+# År
+$year='2022'
+
+# Medspelare
+$reqPlayer='Patricia'
+
+# IDs för spelen som ska hämtas för Hardcore challenge
+$challengeGameIDs = @('146021','70919','244521','163412','199561','110327','68448','221107','148949','28143','185196')
+
+# IDs för spelen som ska hämtas för Normal challenge
+#$challengeGameIDs = @('36218','164812','244522','2448','161417','50','113294','1927','2243','13223')
+
+# Hämta rad-datat till clipboard
+Get-BGGChallengePlaysForEntry -bggUser $bggUser -gameIDs $challengeGameIDs -year $year -reqPlayer $reqPlayer | clip  
