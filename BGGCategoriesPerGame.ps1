e7ab708295d49f9c502dc02759b43b421d671f28
Andreas Pettersson <psson@users.noreply.github.com>
2022-05-12 23:55:32 +0200

    Categories test script

--- BGGCategoriesPerGame.ps1
+++ BGGCategoriesPerGame.ps1
@@ -1,59 +1,23 @@
-﻿function Get-BGGCategoriesForGames {
-    [cmdletbinding()]
-    param (
-        [parameter(ValueFromPipeline)]$gameIDs
-    )
+﻿<#
+Exploring getting categories for games
+#>
 
-    begin {
-        $categories = @{}
-    }
-
-    process {
-        $thingUri = "https://boardgamegeek.com/xmlapi2/thing?id=$gameID"
-        [xml]$xmlThing = Invoke-WebRequest -Uri $thingUri
-
-        $categoryLinks = $xmlThing | Select-Xml -XPath "//*[@type='boardgamecategory']"
-
-        
-
-        foreach ( $category in $categoryLinks.Node.value ) {
-            try {
-                #Write-host $category
-                $categories.Add($category,'category')
-            } catch [ArgumentException] {
-                # Dublett
-            } catch {
-                Write-Error "Unexpected Error"
-            }
-        }
-    }
-
-    end {
-        return $categories
-    }
-}
+Import-Module -Name psson-BGGAPI -Force
 
 
+<#
+$gameID = '146021'  # Eldritch Horror
+#$gameID = '70919'   # Takenoko
+
+[hashtable]$myCategories = Get-BGGCategoriesForGame -GameID $gameID
+$myCategories.Keys
+#>
 
 
-
-#$challengeGameIDs = @('36218','164812','244522','2448','161417','50','113294','1927','2243','13223')
 $challengeGameIDs = @('36218','164812','244522','2448','161417','50','113294','1927','2243','13223')
+$challengeGameIDs
 
 
-$myCats = $challengeGameIDs | Get-BGGCategoriesForGame
+[hashtable]$myCats = $challengeGameIDs | Get-BGGNumCategoriesForGames -Verbose
 $myCats.Keys | Measure-Object | Select-Object -ExpandProperty count
-
-146021 | Get-BGGCategoriesForGame
-
-$gameID = '146021'
-$thingUri = "https://boardgamegeek.com/xmlapi2/thing?id=$gameID"
-[xml]$xmlThing = Invoke-WebRequest -Uri $thingUri
-
-
-$categoryLinks = $xmlThing | Select-Xml -XPath "//*[@type='boardgamecategory']"
-#$categoryLinks.Node
-
-foreach ( $category in $categoryLinks.Node.value ) {
-    $category
-}
+$myCats
