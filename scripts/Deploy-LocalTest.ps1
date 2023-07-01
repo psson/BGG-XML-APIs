74d277032da90b23ef5ca3ceffacb558136b9d5d
Andreas Pettersson <psson@users.noreply.github.com>
2023-07-02 01:18:03 +0200

    Added path for WinPS

--- scripts/Deploy-LocalTest.ps1
+++ scripts/Deploy-LocalTest.ps1
@@ -1,7 +1,10 @@
 $ModulePath="$env:USERPROFILE\Documents\Powershell\Modules"
+$ModulePathWinPS="$env:USERPROFILE\Documents\WindowsPowershell\Modules"
 $ScriptPath="$env:USERPROFILE\Documents\WorkingPS"
 
 # Copy modules
 Copy-Item -Path .\Modules\* -Destination $ModulePath -Recurse -Force
+# Copy modules to WinPS
+Copy-Item -Path .\Modules\* -Destination $ModulePathWinPS -Recurse -Force
 # Copy scripts
 Copy-Item -Path .\* -Destination $ScriptPath -Filter *.ps1 -Force
